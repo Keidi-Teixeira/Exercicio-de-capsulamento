baac33768a34b22b311d1ee632f2219494b41a3b
Keidi-Teixeira <keidifofa@gmail.com>
2022-09-08 17:51:17 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioEncapsulamento/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioEncapsulamento
+{
+    internal class Account
+    {
+        private int number;
+        private String holder;
+        private double balance;
+
+        public int getNumber() { return this.number; }
+        public String getHolder() { return this.holder; }
+        public void setHolder(String holder) { this.holder = holder; } 
+        public double getBalance() { return balance; }
+
+        public Account(int number, String holder) { this.number = number; this.holder = holder; }
+        public Account(int number, String holder, double initialDeposit) { this.number = number; this.holder = holder; deposit(initialDeposit); }
+
+        public void deposit(double amount) { this.balance += amount; }
+        public void withdraw(double amount) { this.balance -= amount + 5.0; }
+        
+        public String toString()
+        {
+            return "Account " + this.number.ToString() + ", Holder: " + this.holder + ", Balance: $ " + Convert.ToString(this.balance) ;
+            //String.Format("%.2f", this.balance)
+        }
+
+
+    }
+}
+
---
+++ ExercicioEncapsulamento/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ExercicioEncapsulamento;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Account account = null;
+        Console.WriteLine("Enter account number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter account holder: ");
+        String holder = Console.ReadLine();
+
+        Console.WriteLine("Is there an initial deposit (y/n)? ");
+        char response = Convert.ToChar(Console.ReadLine());
+
+        if(response == 'y')
+        {
+            Console.WriteLine("Enter initial deposit value: ");
+            double initialDeposit = Convert.ToDouble(Console.ReadLine());
+            account = new Account(number, holder, initialDeposit);
+        }
+        else if(response == 'n')
+        {
+            account = new Account(number, holder);
+        }
+        else
+        {
+            Console.WriteLine("You can just type 'y' or 'n'!");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Account data: ");
+        Console.WriteLine(account.toString());
+
+        Console.WriteLine();
+        Console.WriteLine("Enter a deposit value: ");
+        double depositValue = Convert.ToDouble(Console.ReadLine());
+        account.deposit(depositValue);
+        Console.WriteLine("Updated account data: ");
+        Console.WriteLine(account.toString());
+
+        Console.WriteLine();
+        Console.WriteLine("Enter a withdraw value: ");
+        double withdrawValue = Convert.ToDouble(Console.ReadLine());
+        account.withdraw(withdrawValue);
+        Console.WriteLine("Updated account data: ");
+        Console.WriteLine(account.toString());
+
+
+    }
+}
